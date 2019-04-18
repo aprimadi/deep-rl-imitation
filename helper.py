722d95bb83930c6c91bc797f19f4b97fed7c66de
Armin Primadi <aprimadi@gmail.com>
2019-04-18 21:41:49 +0700

    Adding ortho initializer

--- helper.py
+++ helper.py
@@ -43,7 +43,37 @@ def print_returns_stats(returns, color='green'):
   print(colored('mean return: %s' % np.mean(returns), color))
   print(colored('std of return: %s' % np.std(returns), color))
 
+def ortho_init(scale=1.0):
+  def _ortho_init(shape, dtype, partition_info=None):
+    #lasagne ortho init for tf
+    shape = tuple(shape)
+    if len(shape) == 2:
+      flat_shape = shape
+    elif len(shape) == 4: # assumes NHWC - Num_samples x Height x Width x Channels
+      flat_shape = (np.prod(shape[:-1]), shape[-1])
+    else:
+      raise NotImplementedError
+    a = np.random.normal(0.0, 1.0, flat_shape)
+    u, _, v = np.linalg.svd(a, full_matrices=False)
+    q = u if u.shape == flat_shape else v # pick the one with the correct shape
+    q = q.reshape(shape)
+    return (scale * q[:shape[0], :shape[1]]).astype(np.float32)
+  return _ortho_init
+
 def lstm(xs, ms, s, scope, nh, init_scale=1.0):
+  """LSTM Cell.
+
+  Arguments:
+    xs: Input tensor.
+    ms: Mask tensor, useful to implement dropout, if 1 means to dropout.
+    s: State tensor (memory in LSTM, contains h[t-1] and c[t-1]).
+    scope: Variable scope.
+    nh: Size of hidden layer.
+    init_scale: Scalar value used to initialize weight using ortho_init.
+
+  Returns:
+    Tensor, output of softmax transformation.
+  """
   nbatch, nin = [v.value for v in xs[0].get_shape()]
   with tf.variable_scope(scope):
     wx = tf.get_variable("wx", [nin, nh*4], initializer=ortho_init(init_scale))
